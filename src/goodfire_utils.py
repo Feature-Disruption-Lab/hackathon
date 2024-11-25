a65ea751a68d08236f7b209985338d8cce7aa753
JacksonKaunismaa <jackkaunis@gmail.com>
2024-11-24 20:08:31 -0500

    Add temperature 0

--- src/goodfire_utils.py
+++ src/goodfire_utils.py
@@ -1,16 +1,24 @@
 from scipy.sparse import csr_matrix
 import goodfire
 
-def run_conversation_through_goodfire(conversation: dict, variant: goodfire.Variant, client: goodfire.Client, activations=True, max_tokens=40) -> dict:
+
+def run_conversation_through_goodfire(
+    conversation: dict,
+    variant: goodfire.Variant,
+    client: goodfire.Client,
+    activations=True,
+    max_tokens=40,
+) -> dict:
     variant.reset()
 
     response = ""
 
     for token in client.chat.completions.create(
-            conversation,
-            model=variant,
-            stream=True,
-            max_completion_tokens=max_tokens,
+        conversation,
+        model=variant,
+        stream=True,
+        max_completion_tokens=max_tokens,
+        temperature=0,
     ):
         response += token.choices[0].delta.content
 
@@ -28,14 +36,12 @@ def run_conversation_through_goodfire(conversation: dict, variant: goodfire.Vari
             "features": dense_activations,
         }
     else:
-        return {
-            "prompt": conversation,
-            "response": response
-        }
+        return {"prompt": conversation, "response": response}
+
 
 def clone_variant(variant: goodfire.Variant):
     new_variant = goodfire.Variant(variant.base_model)
     for edit in variant.edits:
-        new_variant.set(edit[0], edit[1]['value'], mode=edit[1]['mode'])
+        new_variant.set(edit[0], edit[1]["value"], mode=edit[1]["mode"])
 
     return new_variant
